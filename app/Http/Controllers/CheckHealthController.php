e89b7a0dd446613e10ceba9bc16320650206c6c5
Bohou007 <bohoussoupaulelie@gmail.com>
2022-09-09 17:24:00 +0000

    Keiwa form final deploiemeent

--- app/Http/Controllers/CheckHealthController.php
+++ app/Http/Controllers/CheckHealthController.php
@@ -8,6 +8,13 @@ class CheckHealthController extends Controller
 {
     public function index()
     {
-        return "Okay";
+        try {
+            return response('Health is Okay !', 200)
+                ->header('Content-Type', 'application/json');
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response($th, 500)
+                ->header('Content-Type', 'application/json');
+        }
     }
 }
